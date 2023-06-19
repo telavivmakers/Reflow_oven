572da6cfc52bdaec9ab2d44a5975ff4c73b074cc
devdesk <tami@tami.org.il>
2023-06-19 23:34:18 +0300

    with screen

--- reflow_toaster/reflow_toaster.ino
+++ reflow_toaster/reflow_toaster.ino
@@ -7,16 +7,26 @@ TAMI Reflow Controller
 // #include <Servo.h>
 // Servo myservo; // Initialize the SERVO library
 
-//assign Arduino pins
-#define LED2 2         // External LED Pin
-#define LED13 13       // Arduino onboard LED Pin
-#define CS 11          //MAX6675 Chip Select
-#define SO 10          //MAX6675 Serial data Output
-#define SCLK 12        //MAX6675 Clock
-#define oven 3         //S.S.R control
-#define door_control 9 //oven door control servo motor
+// OLED
+#include <U8x8lib.h>
+U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* reset=*/U8X8_PIN_NONE); // https://github.com/olikraus/u8g2/wiki/u8x8setupcpp
 
-//declare variables
+// assign Arduino pins
+
+// connect OLED SCK --> A5
+// connectr OLED SDA --> A4
+
+#define LED2 2   // External LED Pin
+#define LED13 13 // Arduino onboard LED Pin
+
+#define CS 11   // MAX6675 Chip Select
+#define SO 10   // MAX6675 Serial data Output a.k.a MISO
+#define SCLK 12 // MAX6675 Clock a.k.a SCK, CLK
+
+#define oven_SSR_pin 3 // S.S.R control
+// #define door_control 9 //oven door control servo motor
+
+// declare variables
 float temperature;
 int delta;
 unsigned long last_time;
@@ -26,38 +36,44 @@ int state = 1;      // There are 5 states: 1. ramp to soak, 2. soak, 3. ramp to 
 int soak_start;     // variable to hold start of soak state time.
 int peak_start;     // variable to hold start of peak state time.
 int state_start;    // variable to hold start of state time.
-int tick_count = 0; //temperature read event counter. increments every 0.25 sec.
+int tick_count = 0; // temperature read event counter. increments every 0.25 sec.
 float integral = 0; // I element of the PID
 
-//define names
+// define names
 #define delta_temperature 12 // Start slowing the oven power "delta_temperature" Degrees before reaching soak temperature. This eliminates temperature overshot.
-//adjust "delta_temperature" so that the overshot is between 5 to 10 degrees. high "delta_temperature" yields low overshot.
-#define MAX6675_time 250  // The minimum MAX6675 temperature measuring time is 220 mili-sec. We will use 250 mili-sec.
-#define closeOvenDoor 1   // Servo angle for closed door
-#define openOvenDoor 140  // Servo angle for opened door
+// adjust "delta_temperature" so that the overshot is between 5 to 10 degrees. high "delta_temperature" yields low overshot.
+#define MAX6675_time 250 // The minimum MAX6675 temperature measuring time is 220 mili-sec. We will use 250 mili-sec.
+#define closeOvenDoor 1  // Servo angle for closed door
+#define openOvenDoor 140 // Servo angle for opened door
 #define soak_temp 150    // profile parameter
 #define soak_duration 75 // profile parameter
-#define peak_temp 230     // profile parameter
+#define peak_temp 230    // profile parameter
 // profile parameter
 
 void setup()
 {
-  //Set timer2 to provide PWM on Pin 3
+  // Set timer2 to provide PWM on Pin 3
   //==================================
-  TCCR2A = (1 << COM2B1);                           //Clear OC2B on Compare Match when up-counting. Set OC2B on Compare Match when down-counting.
+  TCCR2A = (1 << COM2B1);                           // Clear OC2B on Compare Match when up-counting. Set OC2B on Compare Match when down-counting.
   TCCR2A = TCCR2A | (1 << WGM20);                   // Set PWM, Phase Correct mode with TOP = 0xFF (WGM21 and WGM22 are zero by default).
-  TCCR2B = (1 << CS22) | (1 << CS21) | (1 << CS20); //Select clkT2S/1024 From Prescaler.
-  OCR2B = 255;                                      //oven "ON" with full power. OCR2B value controls the oven duty cycle between 0 to 100%. 255 sets oven power to 100%.
+  TCCR2B = (1 << CS22) | (1 << CS21) | (1 << CS20); // Select clkT2S/1024 From Prescaler.
+  OCR2B = 255;                                      // oven "ON" with full power. OCR2B value controls the oven duty cycle between 0 to 100%. 255 sets oven power to 100%.
   //==================================
 
-  Serial.begin(19200);
+  Serial.begin(115200);
   // myservo.attach(door_control); // attaches the door control servo to the servo object
-  pinMode(oven, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED13, OUTPUT);
+  pinMode(oven_SSR_pin, OUTPUT);
+
   pinMode(CS, OUTPUT);
   pinMode(SO, INPUT);
   pinMode(SCLK, OUTPUT);
+
+  pinMode(LED2, OUTPUT);
+  pinMode(LED13, OUTPUT);
+
+  // OLED setup
+  u8x8.begin();
+  u8x8.setFont(u8x8_font_px437wyse700a_2x2_r);
 
   // Read the temperature for the 1st time
   //======================================
@@ -80,6 +96,33 @@ void loop()
     // Serial.print(tick_count++);
     Serial.print(" ; Oven temperature = ");
     Serial.print(temperature);
+
+    // u8x8.setCursor(0, 1);
+    // u8x8.print("STAGE: ");
+    // u8x8.setCursor(12, 1);
+    // u8x8.print(counter);
+
+    int row = 0;
+    u8x8.setCursor(0, row);
+    u8x8.print(" AtTami ");
+
+    row += 2;
+    u8x8.setCursor(0, row);
+    u8x8.print("--------");
+
+    row += 2;
+    u8x8.setCursor(0, row);
+    u8x8.print("Temp");
+
+    u8x8.setCursor(11, row);
+    u8x8.print(int(temperature));
+
+    row += 2;
+    u8x8.setCursor(0, row);
+    u8x8.print("State");
+
+    u8x8.setCursor(11, row);
+    u8x8.print(state);
   }
 
   switch (state)
@@ -98,27 +141,29 @@ void loop()
     }
     delta = soak_temp - temperature;
     if (delta < delta_temperature)
-    { //simple PID control
+    { // simple PID control
       integral = integral + 1;
       delta = map(delta, 0, delta_temperature, 128, 255) + integral;
-      OCR2B = delta; //oven "ON" with proportional to delta partial power
+      OCR2B = delta; // oven "ON" with proportional to delta partial power
     }
     else
-      OCR2B = 255; //oven "ON" full power
+      OCR2B = 255; // oven "ON" full power
+
     Serial.print(" ; state time = ");
     Serial.print(millis() / 1024 - state_start);
     Serial.println(" ; State 1, Ramp to Soak");
+
     break;
 
   case 2: // Soak
     if (temperature < soak_temp)
     {
-      OCR2B = 100; //oven "ON" with partial power
+      OCR2B = 100; // oven "ON" with partial power
       digitalWrite(LED2, HIGH);
     }
     else
     {
-      OCR2B = 0; //oven "OFF"
+      OCR2B = 0; // oven "OFF"
       digitalWrite(LED2, LOW);
     }
     if ((millis() / 1024 - soak_start) > soak_duration)
@@ -135,7 +180,7 @@ void loop()
   case 3: // Ramp to Peak
     if (temperature < peak_temp)
     {
-      OCR2B = 255; //oven "ON" with  power
+      OCR2B = 255; // oven "ON" with  power
       digitalWrite(LED2, HIGH);
     }
     else
@@ -144,7 +189,6 @@ void loop()
       // digitalWrite(LED2, LOW);
       state = 4;
     }
-    
 
     Serial.print(" ; state time = ");
     Serial.print(millis() / 1024 - state_start);
@@ -179,14 +223,15 @@ void loop()
   toggle = ~toggle;
 
   /*
-	Enter final code here
-   	*/
+  Enter final code here
+    */
 
   while ((millis() - last_time) < MAX6675_time)
     ; // wait for MAX6657 temperature measurement
   last_time = millis();
   temperature = read_MAX6657();
   temperature = temperature / 4;
+
 }
 
 int read_MAX6657()
